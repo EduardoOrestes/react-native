5ca6b18ee3c8ed5f699b026c3c331ebe848d2cb3
eduardoorestes <eduardoobrum@gmail.com>
2023-05-26 21:54:34 -0300

    Learning

--- App.js
+++ App.js
@@ -1,41 +1,14 @@
-import React from 'react';
-import {FlatList, StyleSheet, Text, View} from 'react-native';
+import React from "react";
+import {View, Text} from 'react-native';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 22,
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-    backgroundColor: '#e2e8f0',
-  },
-});
-
-const FlatListBasics = () => {
+const Component = () => {
   return (
-    <View style={styles.container}>
-      <FlatList
-        data={[
-          {key: 'Devin'},
-          {key: 'Dan'},
-          {key: 'Dominic'},
-          {key: 'Jackson'},
-          {key: 'James'},
-          {key: 'Joel'},
-          {key: 'John'},
-          {key: 'Jullian'},
-          {key: 'Jimmy'},
-          {key: 'Julie'},
-        ]}
-        renderItem={({item}) => <Text style={styles.item}>
-                                  {item.key}
-                                </Text>}
-      />
+    <View>
+      <Text>
+        Component...
+      </Text>
     </View>
-  );
+  )
 };
 
-export default FlatListBasics;
+export default Component;
