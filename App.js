882a1a3414e23f288bd97880f342dc63ce73e94b
eduardoorestes <eduardoobrum@gmail.com>
2023-07-20 21:34:37 -0300

    Lesson Button

--- App.js
+++ App.js
@@ -1,43 +1,89 @@
 import React from 'react';
-import {View, StyleSheet, Text, useWindowDimensions} from 'react-native';
+import {
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+} from 'react-native';
 
-const App = () => {
-  const {height, width, scale, fontScale } = useWindowDimensions();
+const Separator = () => <View style={styles.separator} />;
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.header}>
-        Window Dimension Data
+const App = () => (
+  <SafeAreaView style={styles.container}>
+    <View>
+      <Text style={styles.title}>
+        O titulo e o manipulador onPress sao obrigatorios.
+        Recomenda-se defini accessibilidadeLabel para ajudar a tornar seu aplicativo utilizavel por todos.
       </Text>
-
-      <Text>
-        Height: {height}
-      </Text>
-
-      <Text>
-        Width: {width}
-      </Text>
-
-      <Text>
-        Font scale: {fontScale}
-      </Text>
-
-      <Text>
-        Pixel ratio: {scale}
-      </Text>
+      <Button
+        title='Press me'
+        onPress={() => Alert.alert('Simple Button Pressed')} />
     </View>
-  );
-};
+
+    <Separator />
+
+    <View>
+      <Text style={styles.title}>
+        Ajuste a cor de uma forma que pareca padrao em cada plataforma.
+        No iOS,o suporte de cor controla a cor do text.
+        No Android, a cor ajusta a cor de fundo do botao.
+      </Text>
+      <Button
+        title="Press me"
+        color="#f194FF"
+        onPress={() => Alert.alert('Button with adjusted color pressed.')} />
+    </View>
+
+    <Separator />
+
+    <View>
+      <Text style={styles.title}>
+        Todas as interacoes para o componente estao desativadas.
+      </Text>
+      <Button
+        title="Press me"
+        disabled
+        onPress={() =>Alert.alert('Cannot press this one')} />
+    </View>
+
+    <Separator />
+
+    <View>
+      <Text style={styles.title}>
+        Essa estrategia de layout permite que o titulo defina a largura do botao.
+      </Text>
+      <View style={styles.fixToText}>
+        <Button
+          title="Left button"
+          onPress={() => Alert.alert('Left button pressed')} />
+        <Button
+          title="Right button"
+          onPress={() => Alert.alert('Right button pressed')} />
+      </View>
+    </View>
+  </SafeAreaView>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    marginHorizontal: 16,
   },
-  header: {
-    fontSize: 20,
-    marginBottom: 12,
+  title: {
+    textAlign: 'center',
+    marginVertical: 8,
+  },
+  fixToText: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: '#737373',
+    borderBottomWidth: StyleSheet.hairlineWidth,
   },
 });
 
