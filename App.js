fdcb1bbb418513d89ebab76e874f80068c9758d4
eduardoorestes <eduardoobrum@gmail.com>
2023-07-20 21:19:59 -0300

    Lesson Style Sheet

--- App.js
+++ App.js
@@ -1,173 +1,43 @@
 import React from 'react';
-import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {View, StyleSheet, Text, useWindowDimensions} from 'react-native';
 
-const App = () => (
-  <SafeAreaView style={styles.container}>
-    <ScrollView contentContainerStyle={styles.scrollContentContainer}>
-      <View style={styles.box}>
-        <Text style={styles.text}>
-          Original Object
-        </Text>
-      </View>
+const App = () => {
+  const {height, width, scale, fontScale } = useWindowDimensions();
 
-      <View
-        style={[
-          styles.box,
-          {
-            transform: [{scale: 2}],
-          },
-        ]}>
-        <Text style={styles.text}>
-          Scale by 2
-        </Text>
-      </View>
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>
+        Window Dimension Data
+      </Text>
 
-      <View
-        style={[
-          styles.box,
-          {
-            transform: [{scaleX: 2}],
-          },
-        ]}>
-        <Text style={styles.text}>
-          ScaleX by 2
-        </Text>
-      </View>
+      <Text>
+        Height: {height}
+      </Text>
 
-      <View
-        style={[
-          styles.box,
-          {
-            transform: [{scaleY: 2}],
-          },
-        ]}>
-        <Text style={styles.text}>
-          ScaleY by 2
-        </Text>
-      </View>
+      <Text>
+        Width: {width}
+      </Text>
 
-      <View
-        style={[
-          styles.box,
-          {
-            transform: [{rotate: '45deg'}],
-          },
-        ]}>
-        <Text style={styles.text}>
-          Rotate by 45 deg
-        </Text>
-      </View>
+      <Text>
+        Font scale: {fontScale}
+      </Text>
 
-      <View
-        style={[
-          styles.box,
-          {
-            transform: [{rotateX: '45deg'}, {rotateZ: '45deg'}],
-          },
-        ]}>
-        <Text style={styles.text}>
-          Rotate X&Z by 45 deg
-        </Text>
-      </View>
-
-      <View
-        style={[
-          styles.box,
-          {
-            transform: [{rotateY: '45deg'}, {rotateZ: '45deg'}],
-          },
-        ]}>
-        <Text style={styles.text}>
-          Rotate Y&Z by 45 deg
-        </Text>
-      </View>
-
-      <View
-        style={[
-          styles.box,
-          {
-            transform: [{skewX: '45deg'}],
-          },
-        ]}>
-        <Text style={styles.text}>
-          SkewX by 45 deg
-        </Text>
-      </View>
-
-      <View
-        style={[
-          styles.box,
-          {
-            transform: [{skewY: '45deg'}],
-          },
-        ]}>
-        <Text style={styles.text}>
-          SkewY by 45 deg
-        </Text>
-      </View>
-
-      <View
-        style={[
-          styles.box,
-          {
-            transform: [{skewX: '30deg'}, {skewY: '30deg'}],
-          },
-        ]}>
-        <Text style={styles.text}>
-          Skew X&Y by 30 deg
-        </Text>
-      </View>
-
-      <View
-        style={[
-          styles.box,
-          {
-            transform: [{translateX: -50}],
-          },
-        ]}>
-        <Text style={styles.text}>
-          Translatex by -50
-        </Text>
-      </View>
-
-      <View
-        style={[
-          styles.box,
-          {
-            transform: [{translateY: 50}],
-          },
-        ]}>
-        <Text style={styles.text}>
-          TranslateY by 50
-        </Text>
-      </View>
-    </ScrollView>
-  </SafeAreaView>
-);
+      <Text>
+        Pixel ratio: {scale}
+      </Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  scrollContentContainer: {
-    alignItems: 'center',
-    paddingBottom: 60,
-  },
-  box: {
-    height: 100,
-    width: 100,
-    borderRadius: 5,
-    marginVertical: 40,
-    backgroundColor: '#61dafb',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
-  text: {
-    fontSize: 14,
-    fontWeight: 'bold',
-    margin: 8,
-    color: '#000',
-    textAlign: 'center',
+  header: {
+    fontSize: 20,
+    marginBottom: 12,
   },
 });
 
