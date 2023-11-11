3a0bd01783f2ba56e71a35eaaece7dde43a0de4e
EduardoOrestes <eduardoobrum@gmail.com>
2023-11-11 16:09:44 -0300

    Added toggle status bar and change status bar style

--- App.js
+++ App.js
@@ -8,32 +8,20 @@ import {
   Text,
   View,
 } from 'react-native';
-import type {StatusBarStyle} from 'react-native';
 
-const STYLES = ['default', 'dark-content', 'light-content'];
+const STYLES = ['deafult', 'dark-content', 'light-content'];
 const TRANSITIONS = ['fade', 'slide', 'none'];
-
-const App = () => {
+ const App = () => {
   const [hidden, setHidden] = useState(false);
   const [statusBarStyle, setStatusBarStyle] = useState(STYLES[0]);
   const [statusBarTransition, setStatusBarTransition] = useState(TRANSITIONS[0]);
   const changeStatusBarVisibility = () => setHidden(!hidden);
-
   const changeStatusBarStyle = () => {
     const styleId = STYLES.indexOf(statusBarStyle) + 1;
     if (styleId === STYLES.length) {
       setStatusBarStyle(STYLES[0]);
     } else {
       setStatusBarStyle(STYLES[styleId]);
-    }
-  };
-
-  const changeStatusBarTransition = () => {
-    const transition = TRANSITIONS.indexOf(statusBarTransition) + 1;
-    if (transition === TRANSITIONS.length) {
-      setStatusBarTransition(TRANSITIONS[0]);
-    } else {
-      setStatusBarTransition(TRANSITIONS[transition]);
     }
   };
 
@@ -44,24 +32,25 @@ const App = () => {
         backgroundColor="#61dafb"
         barStyle={statusBarStyle}
         showHideTransition={statusBarTransition}
-        hidden={hidden} />
+        hidden={hidden}
+      />
       <Text style={styles.textStyle}>
-        StatusBar Visibility:{'\n'}
+        StatusBar Visibility: {'\n'}
         {hidden ? 'Hidden' : 'Visible'}
       </Text>
       <Text style={styles.textStyle}>
-        StatusBar Style:{'\n'}
+        StatusBar Style: {'\n'}
         {statusBarStyle}
       </Text>
       {Platform.OS === 'ios' ? (
         <Text style={styles.textStyle}>
           StatusBar Transition: {'\n'}
-          {statusBarTransition}
+          {statusBarTrantision}
         </Text>
       ) : null}
       <View style={styles.buttonsContainer}>
         <Button title="Toggle StatusBar" onPress={changeStatusBarVisibility} />
-        <Button title="Change StatusBar Style" onPress={changeStatusBarStyle} />
+        <Button title="Chnage StatusBar Style" onPress={changeStatusBarStyle} />
         {Platform.OS === 'ios' ? (
           <Button
             title="Change StatusBar Transition"
