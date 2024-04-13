3a22256adf9652e9191ae5fe7a8adbadd878f8cb
Eduardo Orestes <eduardoobrum@gmail.com>
2024-04-13 12:24:34 -0300

    Navegacao 028

--- rocketseat/navecacao-028/App.tsx
+++ rocketseat/navecacao-028/App.tsx
@@ -1,3 +1,5 @@
+// VIDEO YOUTUBE - https://www.youtube.com/watch?v=Bbi4WItP-Uo&ab_channel=Rocketseat
+
 import 'react-native-gesture-handler';
 
 import { Routes } from './src/routes' // por padrao ele importa o arquivo index, caso contrario tem que colocar o noe do arquivo
