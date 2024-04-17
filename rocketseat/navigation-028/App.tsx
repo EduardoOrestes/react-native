ceca4863b8f3b96b5167c87d570259513c326d54
Eduardo Orestes <eduardoobrum@gmail.com>
2024-04-16 21:39:35 -0300

    Added navigation 028

---
+++ rocketseat/navigation-028/App.tsx
@@ -0,0 +1,11 @@
+// VIDEO YOUTUBE - https://www.youtube.com/watch?v=Bbi4WItP-Uo&ab_channel=Rocketseat
+
+import 'react-native-gesture-handler';
+
+import { Routes } from './src/routes' // por padrao ele importa o arquivo index, caso contrario tem que colocar o noe do arquivo
+
+export default function App() {
+  return (
+    <Routes />
+  );
+}
---
+++ rocketseat/navigation-028/src/@types/navigation.d.ts
@@ -0,0 +1,10 @@
+export declare global {
+  nampespace ReactNavigation {
+    interface RootParamList {
+      screenA: undefined;
+      screenB: {
+        name: string;
+      };
+    }
+  }
+}
